a640b04be7f3fe0b0a709bb66505f18bbd6e5189
guliyevashahjahan <shahjahaneg@code.edu.az>
2023-08-15 15:56:10 +0400

    Create MVC App

---
+++ BigOn backend/BigOn WebUI/Program.cs
@@ -0,0 +1,33 @@
+using BigOn_WebUI.Models.Persistences;
+using Microsoft.EntityFrameworkCore;
+
+namespace BigOn_WebUI
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            builder.Services.AddControllersWithViews();
+            builder.Services.AddDbContext<DataContext>(
+            cfg =>
+            {
+                cfg.UseSqlServer(builder.Configuration.GetConnectionString("cString"));
+
+
+            });
+            var app = builder.Build();
+
+            app.UseStaticFiles();
+            app.UseRouting();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute("default", "{controller=home}/{action=index}/{id?}");
+            });
+          
+
+            app.Run();
+        }
+    }
+}
